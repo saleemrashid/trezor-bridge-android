522fddb98cea95cdf8901f790a7c22de717924b0
Saleem Rashid <trezor@saleemrashid.com>
2016-12-19 13:50:17 +0000

    CustomTabs: Bind to service and warm up

--- app/src/main/java/com/saleemrashid/trezor/bridge/MainActivity.java
+++ app/src/main/java/com/saleemrashid/trezor/bridge/MainActivity.java
@@ -1,16 +1,24 @@
 package com.saleemrashid.trezor.bridge;
 
+import android.content.ComponentName;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final Uri WALLET_URI = Uri.parse("https://wallet.trezor.io/");
+
+    private CustomTabsSession mSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +32,43 @@ public class MainActivity extends AppCompatActivity {
                 openWallet();
             }
         });
+
+        bindCustomTabs();
     }
 
+    /**
+     * Bind Custom Tabs and warm up URI.
+     */
+    private void bindCustomTabs() {
+        final CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
+                Log.i(TAG, "onCustomTabsServiceConnected");
+
+                client.warmup(0);
+
+                mSession = client.newSession(null);
+                mSession.mayLaunchUrl(WALLET_URI, null, null);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                Log.i(TAG, "onServiceDisconnected");
+
+                mSession = null;
+            }
+        };
+
+        CustomTabsClient.bindCustomTabsService(this, CustomTabsHelper.getPackageNameToUse(this), connection);
+    }
+
+    /**
+     * Open wallet URI with Custom Tabs.
+     */
     private void openWallet() {
-        final CustomTabsIntent intent = new CustomTabsIntent.Builder()
+        Log.i(TAG, "openWallet");
+
+        final CustomTabsIntent intent = new CustomTabsIntent.Builder(mSession)
                 .setToolbarColor(ContextCompat.getColor(this, R.color.primary))
                 .setShowTitle(true)
                 .setStartAnimations(this, R.anim.slide_in_right, R.anim.slide_out_left)
